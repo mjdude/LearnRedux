883c27b8693cdcf239fe979df0ece4339226f5c7
mjdude <mo@syncspot.net>
2016-07-05 00:40:07 -0700

    added movies to state

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -2,7 +2,15 @@ var redux = require('redux');
 
 console.log('starting redux example');
 
- var reducer = (state = {name: 'Anonymous'}, action) => {
+var stateDefault = {
+  name: 'Anonymous',
+  hobbies: [],
+  movies: [],
+};
+
+var nextHobbyId = 1;
+var nextMovieId = 1;
+var reducer = (state = stateDefault, action) => {
   //  state = state || {name: 'Anonymous'};
 
   switch (action.type) {
@@ -10,6 +18,29 @@ console.log('starting redux example');
       return {
           ...state,
           name: action.name,
+      };
+    case 'ADD_HOBBY':
+      return {
+        ...state,
+        hobbies: [
+          ...state.hobbies,
+          {
+            id: nextHobbyId++,
+            hobby: action.hobby,
+          }
+        ]
+      };
+    case 'ADD_MOVIE':
+      return {
+        ...state,
+        movies: [
+          ...state.movies,
+          {
+            id: nextMovieId++,
+            title: action.title,
+            genre: action.genre,
+          }
+        ],
       };
     default:
       return state;
@@ -24,6 +55,8 @@ var unsubscribe = store.subscribe(() => {
 
   console.log('name is', state.name);
   document.getElementById('app').innerHTML = state.name;
+
+  console.log('new state' ,store.getState());
 });
 
 // unsubscribe();
@@ -37,9 +70,18 @@ store.dispatch({
   name: 'Mo',
 });
 
-
+store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'running',
+});
 
 store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily',
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'GATTACA',
+  genre: 'sci-fi',
 });
