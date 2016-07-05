80f9d242379c707c53c952edb6e28a14287e983a
mjdude <mo@syncspot.net>
2016-07-05 01:26:19 -0700

    add action for adding / removing movies

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -30,6 +30,11 @@ var reducer = (state = stateDefault, action) => {
           }
         ]
       };
+    case 'REMOVE_HOBBY':
+      return {
+        ...state,
+        hobbies: state.hobbies.filter((hobby) => hobby.id !== action.id),
+      };
     case 'ADD_MOVIE':
       return {
         ...state,
@@ -41,6 +46,11 @@ var reducer = (state = stateDefault, action) => {
             genre: action.genre,
           }
         ],
+      };
+    case 'REMOVE_MOVIE':
+      return {
+        ...state,
+        movies: state.movies.filter((movie) => movie.id !== action.id ),
       };
     default:
       return state;
@@ -76,6 +86,16 @@ store.dispatch({
 });
 
 store.dispatch({
+  type: 'ADD_HOBBY',
+  hobby: 'walking',
+});
+
+store.dispatch({
+  type: 'REMOVE_HOBBY',
+  id: 2,
+});
+
+store.dispatch({
   type: 'CHANGE_NAME',
   name: 'Emily',
 });
@@ -83,5 +103,16 @@ store.dispatch({
 store.dispatch({
   type: 'ADD_MOVIE',
   title: 'GATTACA',
-  genre: 'sci-fi',
+  genre: 'Sci-fi',
+});
+
+store.dispatch({
+  type: 'ADD_MOVIE',
+  title: 'Mad Max',
+  genre: 'Action',
+});
+
+store.dispatch({
+  type: 'REMOVE_MOVIE',
+  id: 1,
 });
