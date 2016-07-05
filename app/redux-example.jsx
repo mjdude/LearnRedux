c9f0ed752baaba207617d0799f44d5b7f5f48bb0
mjdude <mo@syncspot.net>
2016-07-05 16:23:36 -0700

    refactor file to create seperate files for action , reducers and the store

--- app/redux-example.jsx
+++ app/redux-example.jsx
@@ -1,112 +1,19 @@
 var redux = require('redux');
 
-console.log('starting redux example');
+var actions = require('./actions/index');
+var store = require('./store/configureStore').configure();
 
-// Name reducer and action generators
-// ---------------------
-var nameReducer = (state = 'Anonymous', action) => {
-  switch (action.type) {
-    case 'CHANGE_NAME':
-      return action.name;
-    default:
-      return state;
-  }
-};
-
-var changeName = (name) => {
-  return {
-    type: 'CHANGE_NAME',
-    name
-  };
-};
-
-// Hobby reducer and action generators
-//-----------------------
-var nextHobbyId = 1;
-var hobbiesReducer = (state = [], action) => {
-  switch (action.type) {
-    case 'ADD_HOBBY':
-      return [
-        ...state,
-        {
-          id: nextHobbyId++,
-          hobby: action.hobby,
-        }
-      ];
-    case 'REMOVE_HOBBY':
-      return state.filter((hobby) => hobby.id !== action.id);
-    default:
-      return state;
-  }
-};
-
-var addHobby = (hobby) => {
-  return {
-    type: 'ADD_HOBBY',
-    hobby
-  };
-};
-
-var removeHobby = (id) => {
-  return {
-    type: 'REMOVE_HOBBY',
-    id
-  };
-};
-
-// Movie reducer and action generators
-//--------------------
-
-var nextMovieId = 1;
-var moviesReducer = (state = [], action) => {
-  switch (action.type){
-    case 'ADD_MOVIE':
-      return  [
-        ...state,
-        {
-          id: nextMovieId++,
-          title: action.title,
-          genre: action.genre,
-        }
-      ];
-    case 'REMOVE_MOVIE':
-      return state.filter((movie) => movie.id !== action.id );
-    default:
-      return state;
-  };
-};
-
-var addMovie = (title, genre) => {
-  return {
-    type: 'ADD_MOVIE',
-    title,
-    genre,
-  };
-};
-
-var removeMovie = (id) => {
-  return {
-    type: 'REMOVE_MOVIE',
-    id
-  };
-}
-var reducer = redux.combineReducers({
-  name: nameReducer,
-  hobbies: hobbiesReducer,
-  movies: moviesReducer,
-});
-
-var store = redux.createStore(reducer, redux.compose(
-  window.devToolsExtension ? window.devToolsExtension() : f => f
-));
+var {changeName, addHobby, removeHobby, addMovie, removeMovie, fetchLocation} = actions;
 
 var unsubscribe = store.subscribe(() => {
   var state = store.getState();
 
-  console.log('name is', state.name);
-  document.getElementById('app').innerHTML = state.name;
-
   console.log('new state' ,store.getState());
+  if (state.map.isFetching) {
+    document.getElementById('app').innerHTML = 'Loading ...';
+  } else if (state.map.url) {
+    document.getElementById('app').innerHTML = '<a href="' + state.map.url + 'target="_blank">View Your Location</a>'
+  }
 });
 
 // unsubscribe();
@@ -114,6 +21,8 @@ var unsubscribe = store.subscribe(() => {
 var currentState = store.getState();
 
 console.log('currentState', currentState);
+
+store.dispatch(fetchLocation());
 
 store.dispatch(changeName('Mo'));
 
